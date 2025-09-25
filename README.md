5b718c3bd22857361d0e0475005119d40f5db37d
Darius Blaszyk <dhkblaszyk@gmail.com>
2025-09-25 19:39:51 +0200

    Add initial implementation of fpLive

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# fplive
-A file watcher for fast CLI development
+# fpLive
+
+fpLive is a simple file-watching utility for Free Pascal. It automatically rebuilds and runs your program whenever changes are detected in your source files.
+
+Think of it as a lightweight "live reloader" for Free Pascal development.
+
+## Installation
+
+Install using [Nova](https://github.com/nova-packager/nova).
+
+```bash
+nova require daar/fplive --dev
+```
+
+## ✨ Features
+- Watches `.pp`, `.pas` and `.inc` source files in your project directory.
+- Detects file changes and recompiles automatically with **fpc**.
+- Runs the compiled program immediately after a successful build.
+- Supports additional `-Fu` and `-FU` compiler options for unit search paths.
+
+## 🚀 Usage
+
+```bash
+./vendor/bin/live ./example/main.pp -Fu./src
+```
+
+1. Start the watcher with your main source file.
+2. Edit and save your pascal files.
+3. fpLive will detect changes, rebuild with `fpc`, and run the program automatically.
+
+Press `Ctrl+C` to stop the watcher.
---
+++ src/live.pp
@@ -0,0 +1,143 @@
+program live;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes,
+  DateUtils,
+  Process,
+  SysUtils;
+
+type
+  TFileWatcher = class
+  private
+    Files: TStringList;
+    Stamps: array of TDateTime;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure AddPath(const APath: string);
+    function HasChanged: boolean;
+  end;
+
+  constructor TFileWatcher.Create;
+  begin
+    Files := TStringList.Create;
+    Files.Sorted := False;
+  end;
+
+  destructor TFileWatcher.Destroy;
+  begin
+    Files.Free;
+    inherited;
+  end;
+
+  procedure TFileWatcher.AddPath(const APath: string);
+  var
+    SR: TSearchRec;
+  begin
+    if FindFirst(IncludeTrailingPathDelimiter(APath) + '*.pp', faAnyFile, SR) = 0 then
+      repeat
+        Files.Add(IncludeTrailingPathDelimiter(APath) + SR.name);
+      until FindNext(SR) <> 0;
+    FindClose(SR);
+
+    if FindFirst(IncludeTrailingPathDelimiter(APath) + '*.pas', faAnyFile, SR) = 0 then
+      repeat
+        Files.Add(IncludeTrailingPathDelimiter(APath) + SR.name);
+      until FindNext(SR) <> 0;
+    FindClose(SR);
+
+   if FindFirst(IncludeTrailingPathDelimiter(APath) + '*.inc', faAnyFile, SR) = 0 then
+      repeat
+        Files.Add(IncludeTrailingPathDelimiter(APath) + SR.name);
+      until FindNext(SR) <> 0;
+    FindClose(SR);
+
+    SetLength(Stamps, Files.Count);
+  end;
+
+  function TFileWatcher.HasChanged: boolean;
+  var
+    i:      integer;
+    FTime:  TDateTime;
+    change: boolean = False;
+  begin
+    Result := False;
+    for i := 0 to Files.Count - 1 do
+      if FileExists(Files[i]) then
+      begin
+        FTime := FileDateToDateTime(FileAge(Files[i]));
+
+        if FTime > Stamps[i] then
+        begin
+          Stamps[i] := FTime;
+          change := True;
+        end;
+      end;
+
+    exit(change);
+  end;
+
+var
+  Watcher: TFileWatcher;
+  i:    integer;
+  SourceFile, ExeFile: string;
+  Args: array of TProcessString;
+  s:    string;
+begin
+  if ParamCount < 1 then
+  begin
+    WriteLn('Usage: live <main.pp> [compiler options]');
+    Halt(1);
+  end;
+
+  SourceFile := ParamStr(1);
+  ExeFile := ChangeFileExt(SourceFile,
+    {$IFDEF WINDOWS}
+ '.exe'
+    {$ELSE}
+    ''
+    {$ENDIF}
+    );
+
+  // Build argument array: first the source file, then the remaining params
+  SetLength(Args, ParamCount);
+  for i := 1 to ParamCount do
+    Args[i - 1] := ParamStr(i);
+
+  Watcher := TFileWatcher.Create;
+  try
+    // Monitor main file dir
+    Watcher.AddPath(ExtractFilePath(SourceFile));
+
+    // Monitor additional -Fu paths
+    for i := 2 to ParamCount do
+      if (Pos('-Fu', ParamStr(i)) = 1) or (Pos('-FU', ParamStr(i)) = 1) then
+        Watcher.AddPath(Copy(ParamStr(i), 4, Length(ParamStr(i))));
+
+    WriteLn('Live build started. Watching for changes... Press Ctrl+C to quit.');
+
+    while True do
+    begin
+      if Watcher.HasChanged then
+      begin
+        WriteLn('--- Change detected. Rebuilding... ---');
+
+        RunCommand('fpc', Args, s);
+        writeln(s);
+
+        if FileExists(ExeFile) then
+        begin
+          WriteLn('--- Running program ---');
+          RunCommand(ExeFile, [], s);
+          writeln(s);
+        end;
+      end;
+      Sleep(500);
+    end;
+
+  finally
+    Watcher.Free;
+  end;
+end.
